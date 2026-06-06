229eaef00db5b970988de9f218be551953a11c52
Harrison-Mueller <MuellerWH@protonmail.com>
2026-06-06 16:36:25 -0400

    Improves Scroll Snapping

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -16,7 +16,7 @@ function Provider({children} : Prov) {
     // const [ currentSection, setCurrentSection ] = useState(0);
     const currentSection = useRef(10);
     // const [ lastScroll, setLastScroll ] = useState(10);
-    const lastScroll = useRef(10);
+    const lastScroll = useRef(0);
     const scrollEnabled = useRef(true);
     const sections = ["home", "about", "projects", "contact"];
 
@@ -27,7 +27,7 @@ function Provider({children} : Prov) {
             behavior: 'instant'
         });
         window.addEventListener('wheel', watchScroll, { passive: false });
-        window.addEventListener('touchmove', watchScroll, { passive: false });
+        // window.addEventListener('touchmove', watchScroll, { passive: false });
         window.addEventListener('keydown', watchScroll, { passive: false });
     }, []);
 
@@ -54,7 +54,7 @@ function Provider({children} : Prov) {
         currentSection.current = section;
     };  
 
-    const watchScroll = (e: WheelEvent | TouchEvent | KeyboardEvent) => {
+    const watchScroll = (e: WheelEvent | KeyboardEvent) => {
         if(!scrollEnabled.current) {
             e.preventDefault();
             return;
@@ -64,18 +64,25 @@ function Provider({children} : Prov) {
         if(!currentSectionElement) {
             return;
         }
-        if(window.scrollY > lastScroll.current) {
-            if(currentSectionElement.getBoundingClientRect().bottom <= window.innerHeight * 0.9) {
+        console.log("WATCHING SCROLL!");
+
+        const scrollChange = e instanceof WheelEvent ? e.deltaY : 
+            ((e.key === 'ArrowDown' || e.key === 'PageDown' || e.key === ' ') ? 1 : -1);
+        if(scrollChange > 0) {
+            if(currentSectionElement.getBoundingClientRect().bottom <= window.innerHeight * 1) {
                 scrollToSection(currentSection.current + 1);
                 e.preventDefault();
+                console.log("scrolling down");
             }
         }
-        if(window.scrollY < lastScroll.current) {
-            if (currentSectionElement.getBoundingClientRect().top >= window.innerHeight * 0.1) {
+        if(scrollChange < 0) {
+            if (currentSectionElement.getBoundingClientRect().top >= window.innerHeight * 0) {
                 scrollToSection(currentSection.current - 1);
                 e.preventDefault();
+                console.log("scrolling up");
             }
         }
+        // console.log(" last scroll: " + lastScroll.current + "current scroll: " + window.scrollY);
         lastScroll.current = window.scrollY;
     }
 
